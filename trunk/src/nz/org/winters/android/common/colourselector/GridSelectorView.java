f2400ea1ff05d7b9ae527af70033b010ca1b70ec
mathew@winters.org.nz <mathew@winters.org.nz@09dfa403-e849-63bb-73f3-d0e98abe2257>
2013-01-07 01:03:34 +0000

    Add license.

--- trunk/src/nz/org/winters/android/common/colourselector/GridSelectorView.java
+++ trunk/src/nz/org/winters/android/common/colourselector/GridSelectorView.java
@@ -1,6 +1,19 @@
-
 package nz.org.winters.android.common.colourselector;
+/*
+ * Copyright 2013 Mathew Winters
 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ vYou may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 import nz.org.winters.android.common.colourselector.GridColorValueView.OnGSVColorChangedListener;
 import android.content.Context;
 import android.util.AttributeSet;
